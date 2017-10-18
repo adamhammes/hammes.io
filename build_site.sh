ed81111b3a4376485d0b090e6e71bc49068c508e
Adam Hammes <adam@hammes.io>
2017-10-17 23:50:33 -0400

    Set up Github pages deployment

--- build_site.sh
+++ build_site.sh
@@ -1,1 +1,26 @@
-hugo --destination /home/adam/site
+#1/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ -n "$(git status --porcelain --ignore-submodules=dirty)" ]; then
+    printf "${RED}Uncommited changes - aborting.\n${NC}"
+    exit 1
+fi
+
+hugo
+
+# Add changes to git
+cd public
+
+msg="Site update on `date`"
+if [ $# -eq 1 ]; then
+    msg="$1"
+fi
+
+git add -A
+git commit -am "$msg"
+git push origin master
+
+printf "${GREEN}Site successfully updated.\n${NC}"
